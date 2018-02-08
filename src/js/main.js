4f757c29fdd27fdba51d8444f5881838c0489014
Marcia Millanao <marcia.millanao@gmail.com>
2018-02-08 12:44:33 -0300

    completo

---
+++ src/js/main.js
@@ -0,0 +1,70 @@
+document.getElementById("btn-login").addEventListener('click', function(){
+  event.preventDefault();
+  
+  //Se borra los textos creados cada vez que se aprete el boton-send
+  document.querySelectorAll("small").forEach(function(small) {small.remove()});
+
+  var inputNombre = document.getElementById('name');
+  var inputPw = document.getElementById('password');
+
+  
+  
+
+  if(validarNombre(inputNombre) && validarPassword(inputPw)){
+    //document.getElementById("btn-login").setAttribute("href","gallery.html"); 
+    window.open('collage.html','_self',false);
+  } else{
+    validarNombre(inputNombre);
+    validarPassword(inputPw);
+  }
+
+});
+
+
+function validarNombre(inputNombre) {    
+  if(inputNombre.value == ""){  
+    
+    var rellenarText = document.createTextNode("Ingresar nombre de usuario");
+    var labelError = document.createElement('small');
+    labelError.classList.add('error');
+    labelError.appendChild(rellenarText);
+    document.getElementById("nombre1").appendChild(labelError);
+    return false;
+      
+  } else {  
+    return true;
+  }  
+} 
+
+
+function validarPassword(inputPw) {   
+  if(inputPw.value == "" || inputPw.value == "123456" || inputPw.value.length < 6){  
+    inputPw.value = "";
+    var rellenarText = document.createTextNode("Ingresar contraseña");
+    var labelError = document.createElement('small');
+    labelError.classList.add('error');
+    labelError.appendChild(rellenarText);
+    document.getElementById("pw1").appendChild(labelError);
+    return false;
+  } else {  
+    return true; 
+  }  
+} 
+
+
+//Drag and drop functions
+
+function drag(ev){ 
+  ev.dataTransfer.setData('text', ev.target.id);
+}
+
+
+function dragDrop(ev){
+  ev.preventDefault(); 
+}
+
+function drop(ev){
+  ev.preventDefault();
+  var foto = ev.dataTransfer.getData('text');
+  ev.target.appendChild(document.getElementById(foto)); 
+}
